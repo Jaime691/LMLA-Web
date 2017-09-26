1f8e7b247cc6f7212726ed1ac74b9767329f2515
jmayorga <jmayorgadev@gmail.com>
2017-09-26 10:41:19 -0500

    Logout event added

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../actions/authActions';
 
 class NavigationBar extends React.Component {
+  logout(e) {
+    e.preventDefault();
+    this.props.logout();
+  }
+
   render() {
+    const { isAuthenticated } = this.props.auth;
+
+    const userLinks = (
+      <ul className="nav navbar-nav navbar-right">
+        <li><a href="#" onClick={this.logout.bind(this)}>Logout</a></li>
+      </ul>
+    );
 
     const guestLinks = (
       <ul className="nav navbar-nav navbar-right">
@@ -19,7 +33,7 @@ class NavigationBar extends React.Component {
           </div>
 
           <div className="collapse navbar-collapse">
-            {guestLinks}
+            {isAuthenticated ? userLinks : guestLinks}
           </div>
         </div>
       </nav>
@@ -27,4 +41,15 @@ class NavigationBar extends React.Component {
   }
 }
 
-export default NavigationBar;
+NavigationBar.propTypes = {
+  auth: React.PropTypes.object.isRequired,
+  logout: React.PropTypes.func.isRequired
+}
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  };
+}
+
+export default connect(mapStateToProps, { logout })(NavigationBar);
